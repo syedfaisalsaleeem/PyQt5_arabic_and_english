bdc3699be8388681fff59d038d9f9de1ce812c1f
SyedFaisalSaleeem <syedfaisalsaleem.100@gmail.com>
2020-05-10 06:05:26 +0500

    second

---
+++ Numeric_keyboard.py
@@ -0,0 +1,5 @@
+from PyQt5.QtGui import QPixmap,QPainter,QFont,QCursor,QMovie,QTextCursor,QColor,QPen
+from PyQt5.QtCore import QThread, pyqtSignal,QTimer,QTime,Qt,QRect
+from PyQt5.QtWidgets import (QPlainTextEdit,QHeaderView,QScroller,QAbstractItemView,QComboBox,QGraphicsDropShadowEffect,QWidget,QMainWindow,QFrame,
+  QApplication, QDialog,QProgressBar, QPushButton,QMdiSubWindow,QTreeWidget,QLabel,QLineEdit,QTreeWidgetItem,QMdiArea,QGraphicsView,QTextEdit)
+from functools import partial
